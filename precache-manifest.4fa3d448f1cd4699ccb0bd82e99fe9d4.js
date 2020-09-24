576c4a75864c17f875ce6b66d53c74afb34124d2
oggyrock <steelo.sun@gmail.com>
2020-09-24 18:53:29 +0300

    Updates

---
+++ precache-manifest.4fa3d448f1cd4699ccb0bd82e99fe9d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca5bceb38725d78ff6559f6abaf66ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a2277a28b5386df863a",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "76f95d3420bb1c47efb1",
+    "url": "./static/js/2.52802c14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.52802c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2277a28b5386df863a",
+    "url": "./static/js/main.94eb1a5a.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
